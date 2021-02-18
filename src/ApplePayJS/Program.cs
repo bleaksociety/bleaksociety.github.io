6d8c22321dfa5948ca5ce94ae57a58bfff1028b7
diepraemiosus <davidaugust1996@outlook.com>
2021-02-18 17:48:50 -0500

    fix?

---
+++ src/ApplePayJS/Program.cs
@@ -0,0 +1,20 @@
+// Copyright (c) Just Eat, 2016. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace JustEat.ApplePayJS
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults((builder) => builder.UseStartup<Startup>());
+    }
+}
